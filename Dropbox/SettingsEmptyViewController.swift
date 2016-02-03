1a21a71a7820cc566cf735e7ac47b272c6b8c662
Jared Volpe <plainspace@gmail.com>
2016-02-02 21:18:00 -0800

    Rename some things and try to add delete button

---
+++ Dropbox/SettingsEmptyViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsEmptyViewController.swift
+//  Dropbox
+//
+//  Created by Jared on 2/2/16.
+//  Copyright © 2016 plainspace. All rights reserved.
+//
+
+import UIKit
+
+class SettingsEmptyViewController: UIViewController {
+    
+    @IBOutlet weak var SettingsEmptyScrollView: UIScrollView!
+    
+    @IBOutlet weak var SettingsEmptySignOutButton: UIButton!
+    
+    
+    @IBAction func SettingsEmptySignOut(sender: UIButton) {
+        
+        // make this button action sign out
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        SettingsEmptyScrollView.contentSize = CGSize(width: 320, height: 972)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
