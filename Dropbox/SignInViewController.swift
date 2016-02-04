6ce52cbaedaa27c14db874e4dd885827c48680b4
Jared Volpe <plainspace@gmail.com>
2016-02-04 07:31:16 -0800

    Add nav dots to welcome and hook up sign in fields and keyboard

--- Dropbox/SignInViewController.swift
+++ Dropbox/SignInViewController.swift
@@ -20,15 +20,71 @@ class SignInViewController: UIViewController {
         // navigationController?.popViewControllerAnimated(true)
     }
     
+    func keyboardWillShow(notification: NSNotification!) {
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        EmailField.becomeFirstResponder()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+
 
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBOutlet weak var SignInButton: UIButton!
+    
+    @IBAction func didPressSignin(sender: UIButton) {
+    
+        if EmailField.text!.isEmpty || PasswordField.text!.isEmpty {
+            
+            SignInButton.enabled = false
+            
+        }
+        else {
+            
+            SignInButton.enabled = true
+            
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var EmailField: UITextField!
+    
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    @IBAction func EmailEditingChanged(sender: AnyObject) {
+        
+        if EmailField.text!.isEmpty || PasswordField.text!.isEmpty {
+            
+           SignInButton.enabled = false
+            
+        }
+        else {
+            
+            SignInButton.enabled = true
+            
+        }
+        
     }
     
 
